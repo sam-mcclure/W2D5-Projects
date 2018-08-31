b38f9e661dca10c0eaf3b8384b599420684283a1
sam-mcclure <samkmcclure@gmail.com>
2018-08-31 14:42:28 -0700

    Finish p01

--- HashMap/lib/p01_int_set.rb
+++ HashMap/lib/p01_int_set.rb
@@ -71,18 +71,30 @@ class ResizingIntSet
   end
 
   def insert(num)
+    if !self[num].include?(num)
+      @count += 1
+      resize! if @count > num_buckets
+      self[num] << num 
+    end
   end
 
   def remove(num)
+    if self[num].include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end 
   end
 
   def include?(num)
+    self[num].any?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    i = num % num_buckets
+    @store[i]
   end
 
   def num_buckets
@@ -90,5 +102,13 @@ class ResizingIntSet
   end
 
   def resize!
+    new_store = Array.new(num_buckets*2) { Array.new }
+    @store.each do |bucket|
+      bucket.each do |el|
+        i = el % new_store.length
+        new_store[i] << el
+      end 
+    end
+    @store = new_store
   end
 end
